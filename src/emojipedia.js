793cbe265a29aa256520c0733ab00609fa5a9fdb
melinawebb <64170051+melinawebb@users.noreply.github.com>
2020-08-02 14:29:40 -0700

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for 'rolling on the floor, laughing'."
+  },
+  {
+    id: 4,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  },
+  {
+    id: 5,
+    emoji: "🔮 ",
+    name: "Crystal Ball",
+    meaning:
+      "What will happen? The crystal ball symbolizes supernatural perceptions. Is used to look ahead and predict the future."
+  },
+  {
+    id: 6,
+    emoji: "💃",
+    name: "Dancing",
+    meaning:
+      "“I want to party” or “I'm so excited!” The dancing woman in the red dress is also often associated with salsa. In general stands for fun and joy."
+  },
+  {
+    id: 7,
+    emoji: "👏 ",
+    name: "Clapping Hands",
+    meaning:
+      "Bravo, very well done! Emoji shows two clapping hands. Is mostly used for expressing consent and appreciation. Can also be used sarcastically, if something truly doesn't deserve any applause!"
+  },
+  {
+    id: 8,
+    emoji: "🏈 ",
+    name: "American Football",
+    meaning:
+      "Touchdown! Popular ball sport in the US. Two teams try to get the ball into the opposing end zone. The emoji is used in connection with the sport, the Superbowl or the United States."
+  },
+  {
+    id: 9,
+    emoji: "💯",
+    name: "Hundred Points Symbol",
+    meaning:
+      "Number 100 in red, double underlined. Can mean: I'm proud, everything is perfect. I agree 100% with it. I got full score in the exam."
+  }
+];
+
+export default emojipedia;
